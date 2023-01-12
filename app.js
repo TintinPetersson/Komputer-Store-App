4bb70c13d53e60260fd65f024e030c891cf604d2
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2023-01-12 20:32:00 +0100

    small fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const workButton = document.querySelector("#workButton");
 const bankButton = document.querySelector("#bankButton");
 const loanButton = document.querySelector("#getALoanButton");
+const repayLoanButton = document.querySelector("#repayLoanButton");
 const dropdownMenu = document.querySelector("#laptopDropdownList");
 
 let payBalance = document.querySelector("#payBalance");
@@ -12,6 +13,7 @@ let loanBalance = document.querySelector("#loanBalance");
 workButton.addEventListener("click", increasePayBalance);
 bankButton.addEventListener("click", transferWorkMoneyToBank);
 loanButton.addEventListener("click", getALoan);
+repayLoanButton.addEventListener("click", repayLoan)
 
 //Functions
 function increasePayBalance() {
@@ -20,9 +22,14 @@ function increasePayBalance() {
 }
 
 function transferWorkMoneyToBank() {
-  bankBalance.innerText = new Intl.NumberFormat("sv-SE", { style: "currency", currency: "SEK",})
-    .format(parseInt(payBalance.innerText) + parseInt(bankBalance.innerText));
-  payBalance.innerText = 0;
+  if(parseInt(loanBalance.innerText) === 0){
+    bankBalance.innerText = new Intl.NumberFormat("sv-SE", { style: "currency", currency: "SEK",})
+      .format(parseInt(payBalance.innerText) + parseInt(bankBalance.innerText));
+    payBalance.innerText = 0;
+  }
+  else if(parseInt(loanBalance.innerText) > 0){
+    loanBalance.innerText = parseInt(loanBalance.innerText) - (parseInt(bankBalance.innerText) * 0.1);
+  }
 }
 
 function getALoan() {
@@ -34,7 +41,7 @@ function getALoan() {
       .format(requestedLoanNumber);
 
     document.querySelector("#hiddenLoan").removeAttribute("hidden");
-    document.querySelector("#repayLoanButton").removeAttribute("hidden");
+    document.querySelector("#repayLoanDiv").removeAttribute("hidden");
 
   } else if (parseInt(loanBalance.innerText) > 0) {
     alert("You already have a loan you need to pay off!");
@@ -42,3 +49,12 @@ function getALoan() {
     alert("That loan is too large!");
   }
 }
+
+// function repayLoan(){
+
+//   loanBalance.innerText = new Intl.NumberFormat("sv-SE", { style: "currency", currency: "SEK",})
+//       .format(parseInt(loanBalance.innerText) - parseInt(payBalance.innerText));
+
+//   payBalance.innerText = parseInt(loanBalance.innerText);
+
+// }
